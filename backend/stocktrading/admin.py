ab703340a2c6966e919255ebf58aadce611ac359
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-09-21 13:21:25 +0100

    Add admin classes

--- backend/stocktrading/admin.py
+++ backend/stocktrading/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Balance, Stock, User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("user_id", "username")
+
+
+class BalanceAdmin(admin.ModelAdmin):
+    list_display = ("user", "balance")
+
+
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("user", "stock_id", "amount")
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Balance, BalanceAdmin)
+admin.site.register(Stock, StockAdmin)
