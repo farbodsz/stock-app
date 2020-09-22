092ec2a5f3bb80a911bad92e2b592a6838b24b40
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-09-22 18:44:35 +0100

    Update backend/urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from backend.stocktrading import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserView, "user")
+router.register(r"balances", views.BalanceView, "balance")
+router.register(r"stocks", views.StockView, "stock")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
