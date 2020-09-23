b0b2c3c3360c698f9a77331a909cdf2b023c735c
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-09-23 18:04:01 +0100

    Add LandingSection

--- frontend/src/landing/LandingPage.js
+++ frontend/src/landing/LandingPage.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "./Header";
+import LandingSection from "./LandingSection";
 
 export default function LandingPage() {
   return (
     <div>
       <Header />
-      <p>Lorem ipsum</p>
+      <LandingSection title="The Game" body="Lorem ipsum ..." alt={false} />
+      <LandingSection title="About Us" body="Lorem ipsum ..." alt={true} />
+      <LandingSection
+        title="Something else"
+        body="Lorem ipsum ..."
+        alt={false}
+      />
     </div>
   );
 }
