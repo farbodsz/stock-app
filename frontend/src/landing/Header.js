f2cff9ab2a6cd559da4fc8b966776734333e5f4d
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-09-23 18:48:44 +0100

    Add signup and login buttons

--- frontend/src/landing/Header.js
+++ frontend/src/landing/Header.js
@@ -8,6 +8,12 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <p className={styles.title}>Stocks Trading Game</p>
+      <a href="#" className={styles.button}>
+        Login
+      </a>
+      <a href="#" className={styles.button}>
+        Sign Up
+      </a>
     </header>
   );
 }
