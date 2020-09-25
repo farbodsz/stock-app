c61d3ec3d5d95a0fcd6ae05e0502187107296383
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-09-25 12:00:45 +0100

    Make h1 tag styling more consistent

--- frontend/src/login/RegisterPage.js
+++ frontend/src/login/RegisterPage.js
@@ -1,11 +1,17 @@
 import React from "react";
+import styles from "./Login.module.scss";
 import Header from "../common/Header";
+import Footer from "../common/Footer";
 
 export default function RegisterPage() {
   return (
     <div>
       <Header />
-      <p>Register</p>
+      <div className={styles.container}>
+        <h1>Register</h1>
+        <p>Test</p>
+      </div>
+      <Footer />
     </div>
   );
 }
