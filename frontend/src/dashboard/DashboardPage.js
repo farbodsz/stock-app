57c82159faa97b48257e0f66680cd523c5118427
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-09-25 13:16:51 +0100

    Add ticker tape to dashboard

--- frontend/src/dashboard/DashboardPage.js
+++ frontend/src/dashboard/DashboardPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./DashboardPage.module.scss";
 import NavigationPane from "./NavigationPane";
+import TickerTapeWidget from "../tradingview/TickerTapeWidget";
 
 export default class DashboardPage extends React.Component {
   getUsername() {
@@ -8,14 +9,33 @@ export default class DashboardPage extends React.Component {
     return "bingbong";
   }
 
+  getTickerTapeSymbols() {
+    // TODO: Dummy
+    return [
+      {
+        proName: "NASDAQ:GOOGL",
+        title: "Google"
+      },
+      {
+        proName: "NASDAQ:TSLA",
+        title: "Tesla"
+      },
+      {
+        proName: "NASDAQ:AMZN",
+        title: "Amazon"
+      }
+    ];
+  }
+
   render() {
     return (
       <div className={styles.containerParent}>
         <div className={styles.containerNav}>
-          <NavigationPane />
+          <NavigationPane className={styles.containerNav} />
         </div>
         <main className={styles.containerMain}>
           <h1>Welcome, {this.getUsername()}!</h1>
+          <TickerTapeWidget symbols={this.getTickerTapeSymbols()} />
         </main>
       </div>
     );
