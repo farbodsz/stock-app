bd1c0f1e7601103b4fb108177a8e4758691b2801
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-09-26 17:27:00 +0100

    Condense imports

--- frontend/src/login/RegisterPage.js
+++ frontend/src/login/RegisterPage.js
@@ -1,12 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "../common/Header";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Container from "@material-ui/core/Container";
+import { Button, Container, Grid, Link, TextField } from "@material-ui/core";
 
-export default class RegisterPage extends Component {
+export default class RegisterPage extends React.Component {
   render() {
     return (
       <div>
