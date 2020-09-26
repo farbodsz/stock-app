810b59a286648716fc41456533ce8620f0b9277b
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-09-26 12:27:50 +0100

    Add stocks chart to dashboard

--- frontend/src/dashboard/HomePane.js
+++ frontend/src/dashboard/HomePane.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./HomePane.module.scss";
 import Card from "./Card";
+import AdvancedRealTimeChartWidget from "../tradingview/AdvancedRealTimeChartWidget";
 
 export default class HomePane extends React.Component {
   getUsername() {
@@ -14,12 +15,19 @@ export default class HomePane extends React.Component {
   }
 
   render() {
+    // TODO: Replace AdvancedRealTimeChartWidget with a symbol overview widget
+    // instead?
     return (
       <div>
         <h1>Welcome, {this.getUsername()}!</h1>
         <div className={styles.balanceContainer}>
           <Card title="Balance">
             <p className={styles.balanceText}>{this.getBalance()}</p>
+          </Card>
+        </div>
+        <div className={styles.chartContainer}>
+          <Card title="Your Stocks">
+            <AdvancedRealTimeChartWidget symbol="NASDAQ:AAPL" width="100%" />
           </Card>
         </div>
       </div>
