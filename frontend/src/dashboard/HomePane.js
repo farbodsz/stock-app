0e02ef6b00e07c59405e5cfc8442a4d4d77a80e6
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-09-26 12:01:20 +0100

    Add balance card to HomePane

--- frontend/src/dashboard/HomePane.js
+++ frontend/src/dashboard/HomePane.js
@@ -1,4 +1,6 @@
 import React from "react";
+import styles from "./HomePane.module.scss";
+import Card from "./Card";
 
 export default class HomePane extends React.Component {
   getUsername() {
@@ -6,7 +8,21 @@ export default class HomePane extends React.Component {
     return "bingbong";
   }
 
+  getBalance() {
+    // TODO: Dummy
+    return "$1 000 000";
+  }
+
   render() {
-    return <h1>Welcome, {this.getUsername()}!</h1>;
+    return (
+      <div>
+        <h1>Welcome, {this.getUsername()}!</h1>
+        <div className={styles.balanceContainer}>
+          <Card title="Balance">
+            <p className={styles.balanceText}>{this.getBalance()}</p>
+          </Card>
+        </div>
+      </div>
+    );
   }
 }
