03e7fa3cfc9cc3e766d4aca3c79c3e05c17dae2e
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-09-26 18:51:55 +0100

    Use styled components in LoginPage for design consistency

--- frontend/src/login/LoginPage.js
+++ frontend/src/login/LoginPage.js
@@ -3,11 +3,14 @@ import styles from "./Login.module.scss";
 import Header from "../common/Header";
 import Button from "../common/Button";
 import TextField from "../common/TextField";
+import Card from "../common/Card";
+import FormLayout from "./FormLayout";
 import axios from "../api/axios";
 
 const headers = {
   "content-type": "application/json"
 };
+
 const headers2 = {
   authorization: "",
   "content-type": "application/json"
@@ -23,6 +26,8 @@ export default class LoginPage extends React.Component {
       token: ""
     };
 
+    // TODO: Temporary bindings
+    this.testAction = this.testAction.bind(this);
     this.onSubmit2 = this.onSubmit2.bind(this);
   }
 
@@ -92,24 +97,32 @@ export default class LoginPage extends React.Component {
     return (
       <div>
         <Header />
-        <div className={styles.container} onSubmit={this.testAction}>
-          <h1>Login</h1>
-          <form noValidate autoComplete="off">
-            <TextField
-              id="username"
-              label="Username"
-              onChange={e => this.setState({ username: e.target.value })}
-            />
-            <TextField
-              id="password"
-              label="Password"
-              type="password"
-              onChange={e => this.setState({ password: e.target.value })}
-            />
-            <Button id="submit" type="submit">
-              Submit
-            </Button>
-          </form>
+        <div>
+          <FormLayout>
+            <Card title="Login">
+              <TextField
+                id="username"
+                className={styles.formElement}
+                label="Username"
+                onChange={e => this.setState({ username: e.target.value })}
+              />
+              <TextField
+                id="password"
+                className={styles.formElement}
+                label="Password"
+                type="password"
+                onChange={e => this.setState({ password: e.target.value })}
+              />
+              <Button
+                id="submit"
+                className={styles.button}
+                onClick={this.testAction}
+              >
+                Submit
+              </Button>
+            </Card>
+          </FormLayout>
+
           <Button id="submit2" onClick={this.onSubmit2}>
             Test Submit
           </Button>
