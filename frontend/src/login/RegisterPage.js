5a7bcb444685f49ffc25d0f7a03bf7a19093f386
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-09-26 18:44:37 +0100

    Add FormLayout and Card to RegisterPage

--- frontend/src/login/RegisterPage.js
+++ frontend/src/login/RegisterPage.js
@@ -3,39 +3,43 @@ import styles from "./Login.module.scss";
 import Header from "../common/Header";
 import Button from "../common/Button";
 import TextField from "../common/TextField";
-import { Container, Grid, Link } from "@material-ui/core";
+import FormLayout from "./FormLayout";
+import Card from "../common/Card";
+import { Grid, Link } from "@material-ui/core";
 
 export default class RegisterPage extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <Container component="main" maxWidth="xs">
-          <TextField
-            className={styles.formElement}
-            name="username"
-            label="Username"
-          />
-          <TextField
-            className={styles.formElement}
-            name="password"
-            label="Password"
-            type="password"
-          />
-          <Button className={styles.button}>Sign In</Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
+        <FormLayout>
+          <Card title="Register">
+            <TextField
+              className={styles.formElement}
+              name="username"
+              label="Username"
+            />
+            <TextField
+              className={styles.formElement}
+              name="password"
+              label="Password"
+              type="password"
+            />
+            <Button className={styles.button}>Sign In</Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </Container>
+          </Card>
+        </FormLayout>
       </div>
     );
   }
