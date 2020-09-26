5d63c9bbadeb8bf9c2ea489c7673d2ee5b8a168b
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-09-26 12:43:34 +0100

    Make dashboard layout adaptive by adding PaneLayout component

--- frontend/src/dashboard/DashboardPage.js
+++ frontend/src/dashboard/DashboardPage.js
@@ -3,6 +3,7 @@ import styles from "./DashboardPage.module.scss";
 import NavigationPane from "./NavigationPane";
 import TickerTapeWidget from "../tradingview/TickerTapeWidget";
 import HomePane from "./HomePane";
+import PaneLayout from "./PaneLayout";
 
 export default class DashboardPage extends React.Component {
   getTickerTapeSymbols() {
@@ -31,8 +32,10 @@ export default class DashboardPage extends React.Component {
         </div>
         <main className={styles.containerMain}>
           <TickerTapeWidget symbols={this.getTickerTapeSymbols()} />
-          <div className={styles.containerPane}>
-            <HomePane />
+          <div className={styles.containerPanes}>
+            <PaneLayout>
+              <HomePane />
+            </PaneLayout>
           </div>
         </main>
       </div>
