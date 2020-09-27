fd635dce8dc43100e190244ad226b245d86a870f
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-09-27 13:37:23 +0100

    Rename Login/Register imports for consistency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import LandingPage from "./landing/LandingPage";
-import Login from "./login/LoginPage";
-import Register from "./login/RegisterPage";
-import Dashboard from "./dashboard/DashboardPage";
+import LoginPage from "./login/LoginPage";
+import RegisterPage from "./login/RegisterPage";
+import Dashboard from "./dashboard/Dashboard";
 
 function App() {
   // TODO: add Error page for Route
@@ -11,8 +11,8 @@ function App() {
     <main>
       <Switch>
         <Route exact path="/" component={LandingPage} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
         <Route path="/dashboard" component={Dashboard} />
       </Switch>
     </main>
